ee61bcdd9b6553243542d0333d13e5742825f043
Masraful Zishan <zishan3231>
2022-09-03 23:08:30 +0600

    blogs-page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const loadMenu = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => fieldMenu(data.data.news_category))
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => fieldMenu(data.data.news_category))
+    }
+    catch (error) {
+        console.log(error)
+    }
     // .then(data => console.log(data.data))
 
 }
@@ -26,11 +31,18 @@ loadMenu()
 
 
 const loadNewsBlogs = () => {
-    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
-        .then(res => res.json())
-        .then(data => displayNews(data.data))
+    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayNews(data.data))
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 const displayNews = news => {
+
     const newsContainer = document.getElementById('news-blogs');
     news.forEach(blog => {
         const newsDiv = document.createElement('div');
@@ -43,11 +55,15 @@ const displayNews = news => {
             <p class="card-text">${blog.details.slice(0, 150) + '...'}</p>
             <div class='d-flex m-2'>
             <img src="${blog.author.img}" class='img-fluid img-width h-100 my-auto rounded-circle' alt="">
-            <div>
-            <h3 class='m-3'>${blog.author.name}</h3>
-            <p class='m-3'>${blog.author.published_date} </p>
-            <h3 class='m-3'>total_view:${blog.total_view}</h3>
-           </div>
+            <div class='d-flex justify-content-between'>
+                <div>
+                    <h3 class='m-3'>${blog.author.name}</h3>
+                    <p class='m-3'>${blog.author.published_date} </p>
+                </div>
+                <div>
+                    <h3 class='m-5'><i class="fa-light fa-eye">${blog.total_view ? blog.total_view : `Not found`}</i></h3>
+                </div>
+            </div>
 
             </div>
         </div>
@@ -61,6 +77,7 @@ const displayNews = news => {
 
     })
 
+
     toggleSpinner(false);
 
 }
@@ -68,33 +85,45 @@ const displayNews = news => {
 function buttonModal(news_id) {
     // console.log(id)
     const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayModal(data.data))
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayModal(data.data))
+    }
+    catch (error) {
+        console.log(error)
+    }
 
 }
 
 
 function displayModal(modaldetalis) {
-    console.log(modaldetalis)
+
     const modalField = document.getElementById('newsDetalis')
     modalField.innerHTML = '';
     modaldetalis.forEach(details => {
         const modal = document.createElement('div');
         modal.innerHTML = `
-        <div class="modal-content container mt-5">
+        <div class="modal-content container mt-5 w-50">
+        <img class='img-fluid' src="${details.thumbnail_url}" alt="">
         <div class="modal-header">
         <h5 class="modal-title d-block" id="newsDetalisLabel">${details.title}</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <p>${details.details}</p>
-        <div class="modal-body d-flex">
+        <div class="modal-body">
         <img src="${details.author.img}" class='img-fluid img-width h-100 my-auto rounded-circle' alt="">
-        <div>
-        <h3 class='m-3'>${details.author.name}</h3>
-        <p class='m-3'>${details.author.published_date} </p>
-        <h3 class='m-3'>total_view:${details.total_view}</h3>
-       </div>
+        <div class='d-flex'>
+            <div>
+                <h3 class='m-3'>${details.author.name}</h3>
+                <p class='m-3'>${details.author.published_date} </p>
+               
+            </div>
+            <div>
+                <h3 class='m-3'><i class="fa-light fa-eye">${details.total_view}</i></h3>
+            </div>
+        </div>
+    </div>
         
         </div>
         <div class="modal-footer">
@@ -117,5 +146,9 @@ const toggleSpinner = isLoading => {
     else {
         loaderSection.classList.add('d-none')
     }
+
+
 }
+
+
 // loadNewsBlogs()
